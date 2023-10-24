57c52d2ec625bcc799a14823bf69fdcd9d55ef05
KondratevI <kondratiev_ur@mail.ru>
2023-10-24 13:24:11 +0300

    added files adn code

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+df = pd.read_csv('vehicles_us.csv')
+st.header('Header')
+st.write('Number of cylinders')
+histogram_cylinders = px.histogram(df, x="cylinders")
+st.plotly_chart(histogram_cylinders, use_container_width=False)
+
+st.write('Relation between condition of a car and its price')
+cheap_cars = st.checkbox('Only cars cheaper than 100,000')
+if cheap_cars:
+    scatterplot = px.scatter(df, x="price", y="condition", color="cylinders", size="price")
+    scatterplot.update_xaxes(range=[0, 100000])
+    st.plotly_chart(scatterplot, use_container_width=False)
+else:
+    scatterplot = px.scatter(df, x="price", y="condition", color="cylinders", size="price")
+    st.plotly_chart(scatterplot, use_container_width=False)
